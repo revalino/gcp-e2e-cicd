c9ee20a38e5f8b67c63c5fbc0f38a5f51d7f8bd8
AlvarDev <alvardev.lp@gmail.com>
2022-02-13 15:27:11 -0300

    Set project-id via gcloud

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -1,21 +1,21 @@
 # Creates 3 GKE autopilot clusters
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
-export PROJECT_ID="project-id-here"
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 # Test cluster
 echo "creating testcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "testcluster" \
+gcloud container --project "$PROJECT_ID" clusters create-auto "testcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Staging cluster
 echo "creating stagingcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "stagingcluster" \
+gcloud container --project "$PROJECT_ID" clusters create-auto "stagingcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Prod cluster
 echo "creating prodcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "prodcluster" \
+gcloud container --project "$PROJECT_ID" clusters create-auto "prodcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
