204444facf0cb90a0538e3373d9141940b8b8895
AlvarDev <alvardev.lp@gmail.com>
2022-02-14 07:20:02 -0300

    Use GKE Standard Mode

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -3,19 +3,19 @@
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 # Test cluster
 echo "creating testcluster..."
-gcloud container --project "$PROJECT_ID" clusters create-auto "testcluster" \
+gcloud container --project "$PROJECT_ID" clusters "testcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Staging cluster
 echo "creating stagingcluster..."
-gcloud container --project "$PROJECT_ID" clusters create-auto "stagingcluster" \
+gcloud container --project "$PROJECT_ID" clusters "stagingcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Prod cluster
 echo "creating prodcluster..."
-gcloud container --project "$PROJECT_ID" clusters create-auto "prodcluster" \
+gcloud container --project "$PROJECT_ID" clusters  "prodcluster" \
 --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
