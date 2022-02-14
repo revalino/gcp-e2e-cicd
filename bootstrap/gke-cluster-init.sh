4177f7cbd705ddb8ba64b01d6dae9e3ef11614d2
AlvarDev <alvardev.lp@gmail.com>
2022-02-14 08:06:05 -0300

    use ZONE instead of REGION

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -4,19 +4,19 @@ export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 # Test cluster
 echo "creating testcluster..."
 gcloud container --project "$PROJECT_ID" clusters create "testcluster" \
---enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
+--enable-ip-alias --zone "us-central1-b" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Staging cluster
 echo "creating stagingcluster..."
 gcloud container --project "$PROJECT_ID" clusters create "stagingcluster" \
---enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
+--enable-ip-alias --zone "us-central1-b" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Prod cluster
 echo "creating prodcluster..."
 gcloud container --project "$PROJECT_ID" clusters create "prodcluster" \
---enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
+--enable-ip-alias --zone "us-central1-b" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 echo "Creating clusters! Check the UI for progress"
