945c776701862ca188a000626508a7016763cf24
AlvarDev <alvardev.lp@gmail.com>
2022-02-14 07:48:47 -0300

    remove --num-nodes (check for futures releases)

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -6,17 +6,17 @@ echo "creating testcluster..."
 gcloud container --project "$PROJECT_ID" clusters create "testcluster" \
 --enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --num-nodes=4 --async 
+--cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Staging cluster
 echo "creating stagingcluster..."
 gcloud container --project "$PROJECT_ID" clusters create "stagingcluster" \
 --enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --num-nodes=4 --async 
+--cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 # Prod cluster
 echo "creating prodcluster..."
 gcloud container --project "$PROJECT_ID" clusters create "prodcluster" \
 --enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --num-nodes=4 --async 
+--cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
 echo "Creating clusters! Check the UI for progress"
