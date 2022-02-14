6aa66377ff62efae12bb91c28f189761e248afd3
AlvarDev <alvardev.lp@gmail.com>
2022-02-14 07:38:56 -0300

    set --num-nodes=4 for each cluster

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -3,20 +3,20 @@
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 # Test cluster
 echo "creating testcluster..."
-gcloud container --project "$PROJECT_ID" clusters "testcluster" \
---region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
+gcloud container --project "$PROJECT_ID" clusters create "testcluster" \
+--enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
+--cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --num-nodes=4 --async 
 # Staging cluster
 echo "creating stagingcluster..."
-gcloud container --project "$PROJECT_ID" clusters "stagingcluster" \
---region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
+gcloud container --project "$PROJECT_ID" clusters create "stagingcluster" \
+--enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
+--cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --num-nodes=4 --async 
 # Prod cluster
 echo "creating prodcluster..."
-gcloud container --project "$PROJECT_ID" clusters  "prodcluster" \
---region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
+gcloud container --project "$PROJECT_ID" clusters create "prodcluster" \
+--enable-ip-alias --region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
+--cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --num-nodes=4 --async 
 echo "Creating clusters! Check the UI for progress"
