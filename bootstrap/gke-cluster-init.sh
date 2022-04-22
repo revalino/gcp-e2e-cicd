a5fb29a94e7aa2d8b8cfa1456d77d5f04465d6a5
AlvarDev <alvardev.lp@gmail.com>
2022-04-22 05:15:29 -0300

    init conf and clusters

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -1,22 +1,36 @@
 # Creates 3 GKE autopilot clusters
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
+export CLUSTER_ZONE=europe-west1-b
+export VPC=vpc-cloud-deploy-techub
+export SUBNET=subnet-cloud-deploy-eu
+
 # Test cluster
 echo "creating testcluster..."
-gcloud container --project "$PROJECT_ID" clusters create "testcluster" \
---enable-ip-alias --zone "us-central1-b" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
---subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
+gcloud container clusters create testcluster \
+	--project=${PROJECT_ID}  \
+	--zone=${CLUSTER_ZONE} \
+	--network=${VPC} \
+	--subnetwork=${SUBNET} \
+	--async 
+
 # Staging cluster
 echo "creating stagingcluster..."
-gcloud container --project "$PROJECT_ID" clusters create "stagingcluster" \
---enable-ip-alias --zone "us-central1-b" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
---subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
+gcloud container clusters create stagingcluster \
+	--project=${PROJECT_ID} \
+	--zone=${CLUSTER_ZONE} \
+	--network=${VPC} \
+	--subnetwork=${SUBNET} \
+	--async 
+
 # Prod cluster
 echo "creating prodcluster..."
-gcloud container --project "$PROJECT_ID" clusters create "prodcluster" \
---enable-ip-alias --zone "us-central1-b" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
---subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
---cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async 
+gcloud container clusters create prodcluster \
+	--project=${PROJECT_ID} \
+	--zone=${CLUSTER_ZONE} \
+	--network=${VPC} \
+	--subnetwork=${SUBNET} \
+	--async 
+
 echo "Creating clusters! Check the UI for progress"
--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,16 +1,27 @@
 # Initilize set project ID variable and run various initializations
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
 # sets the current project for gcloud
 gcloud config set project $PROJECT_ID
+
 # Enables various APIs you'll need
-gcloud services enable container.googleapis.com cloudbuild.googleapis.com \
-artifactregistry.googleapis.com clouddeploy.googleapis.com \
-cloudresourcemanager.googleapis.com
+gcloud services enable \
+	container.googleapis.com \
+	cloudbuild.googleapis.com \
+	artifactregistry.googleapis.com \
+	clouddeploy.googleapis.com \
+	cloudresourcemanager.googleapis.com
+
 # creates the Artifact Registry repo
-gcloud artifacts repositories create hello-world-python --location=us-central1 \
---repository-format=docker
+gcloud artifacts repositories create hello-world-python \
+	--location=europe-west1 \
+	--repository-format=docker
+
 # creates the Google Cloud Deploy pipeline
-gcloud deploy apply --file clouddeploy.yaml \
---region=us-central1 --project=$PROJECT_ID
+gcloud deploy apply \
+	--file clouddeploy.yaml \
+	--region=europe-west1 \
+	--project=$PROJECT_ID
+
 echo "init done. To create clusters, run: ./gke-cluster-init.sh"
